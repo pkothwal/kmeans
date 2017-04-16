c600e9923b8aefd7ea29382fb7ef94f6bd4f7ca3
pkothwal <pkothwal@gmail.com>
2017-04-16 11:12:51 +0530

    initial commit

---
+++ src/main/java/com/prahalad/App.java
@@ -0,0 +1,13 @@
+package com.prahalad;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
